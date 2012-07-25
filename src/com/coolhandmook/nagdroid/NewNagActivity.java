633fa009152f8c6aa5cb5d4f062708256fead099
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-07-25 23:14:28 +0100

    Open the selected application at the selected time.

--- src/com/coolhandmook/nagdroid/NewNagActivity.java
+++ src/com/coolhandmook/nagdroid/NewNagActivity.java
@@ -1,14 +1,19 @@
 package com.coolhandmook.nagdroid;
 
-import java.util.List;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class NewNagActivity extends Activity {
 
@@ -31,15 +36,27 @@ public class NewNagActivity extends Activity {
         return true;
     }
     
-    public void onApplicationOpen(View view) {
+    public void onScheduleNagClick(View view) {
     	Spinner spinner = (Spinner) findViewById(R.id.applicationNameSpinner);
     	int selected = spinner.getLastVisiblePosition();
     	
     	Intent intent = getPackageManager().getLaunchIntentForPackage(viewAdapter.getItem(selected).packageName);
     	if (intent != null)
     	{
-    		startActivity(intent);
+    		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+    		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+    		alarmManager.set(AlarmManager.RTC, timeInMilliseconds(), pendingIntent);
     	}
+    }
+    
+    private long timeInMilliseconds()
+    {
+    	TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+    	Calendar calendar = new GregorianCalendar();
+    	calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
+    	calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+    	calendar.set(Calendar.SECOND, 0);
+    	return calendar.getTimeInMillis();
     }
     
 }
