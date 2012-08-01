0c8bb2aa50d3e31d544f51278e04b6e50300a2a2
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-08-01 23:09:02 +0100

    Merged ShowScheduleActivity into main activity.

--- src/com/coolhandmook/nagdroid/MainActivity.java
+++ src/com/coolhandmook/nagdroid/MainActivity.java
@@ -3,21 +3,82 @@ package com.coolhandmook.nagdroid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private ScheduledViewAdapter viewAdapter;
+	private Database database;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        database = new Database(this);
+    }
+    
+    @Override
+    public void onResume()
+    {
+    	super.onResume();	
+        viewAdapter = new ScheduledViewAdapter(this, R.id.scheduledApplicationName,
+        									   database.findScheduledApplications(Long.MAX_VALUE));
+        
+        ListView list = (ListView) findViewById(R.id.scheduleList);
+        list.setAdapter(viewAdapter);
+        registerForContextMenu(list);
+    }
+    
+    @Override
+    public void onDestroy()
+    {
+    	super.onDestroy();
+        database.close();
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+    	super.onCreateContextMenu(menu, v, menuInfo);
+
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.schedule_item_menu, menu);
+
+    	TextView textView = (TextView) v.findViewById(R.id.scheduledApplicationName);
+    	menu.setHeaderTitle(textView.getText());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public boolean onContextItemSelected(MenuItem item)
+    {
+    	boolean result = false;
+
+    	if (item.getItemId() == R.id.remove_schedule)
+    	{
+    		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+    		ScheduledLaunch launch = viewAdapter.getItem(info.position);
+
+    		Intent scheduleRemove = new Intent(this, NagService.class);
+    		scheduleRemove.setAction(NagService.SCHEDULE_REMOVE);
+    		scheduleRemove.putExtra(NagService.ARG_TIME, launch.time);
+    		scheduleRemove.putExtra(NagService.ARG_PACKAGE, launch.packageName);
+    		startService(scheduleRemove);
+
+    		viewAdapter.remove(launch);
+    		
+    		result = true;
+    	}
+
+    	return result;
     }
 
     public void onCreateNewNagClick(View view)
@@ -25,8 +86,4 @@ public class MainActivity extends Activity {
     	startActivity(new Intent(this, NewNagActivity.class));
     }
 
-    public void onShowScheduleClick(View view)
-    {
-    	startActivity(new Intent(this, ShowScheduleActivity.class));
-    }
 }
